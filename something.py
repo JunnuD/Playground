f4956f7259174003110d626117ab4a06022ef570
JunnuD <junnudanhammer@gmail.com>
2023-12-17 17:59:45 +0200

    First implements to basics

---
+++ something.py
@@ -0,0 +1,27 @@
+def function(for_looppi):
+    for i in range (11):
+        print("I now what im doing. ")
+
+
+def lista():
+    for f in [1,2,3,4,56,880,7758]:
+        print()
+        print("Listan numero: ", f, "Listan numero + 2: ", f+2, "Listan numero * 2: ",)
+        
+def summa():
+    total = 0
+    lista = [120, 504, 62]
+    tulos = sum(lista)
+
+    total = tulos
+    print()
+    print(total)
+
+
+function(range)
+
+lista()
+
+summa()
+
+
---
+++ turtletesting.py
@@ -0,0 +1,21 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("darkred")
+
+notta = turtle.Turtle()
+notta.shape("turtle")
+notta.pensize(3)
+
+for viisari in range (12):
+    notta.penup()
+    notta.forward(180)
+    notta.pendown()
+    notta.forward(15)
+    notta.penup()
+    notta.forward(20)
+    notta.stamp()
+    notta.backward(215)
+    notta.left(360/12)
+
+wn.mainloop()
