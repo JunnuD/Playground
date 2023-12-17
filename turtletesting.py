f57b7434a6e25b495b1c24737e234ce04decceb8
JunnuD <junnudanhammer@gmail.com>
2023-12-17 18:03:02 +0200

    Update turtletesting.py

--- turtletesting.py
+++ turtletesting.py
@@ -1,14 +1,22 @@
 import turtle
-import tkinter
+import tkinter as tk
 
+# Create a tkinter window
+root = tk.Tk()
+root.title("Turtle Graphics")
+root.geometry("400x400")  # Set the window size
+
+# Create a turtle screen
 wn = turtle.Screen()
 wn.bgcolor("darkred")
 
+# Create a turtle
 notta = turtle.Turtle()
 notta.shape("turtle")
 notta.pensize(3)
 
-for viisari in range (12):
+# Draw the clock hands
+for viisari in range(12):
     notta.penup()
     notta.forward(180)
     notta.pendown()
@@ -19,4 +27,4 @@ for viisari in range (12):
     notta.backward(215)
     notta.left(360/12)
 
-wn.mainloop()
+wn.exitonclick()  # Close the turtle graphics window when clicked
